1ee8d767e92fb9a12babae4b764f673b1095d882
sofkprog <94756202+sofkprog@users.noreply.github.com>
2022-02-15 20:47:17 +0300

    капучино

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys
 import sqlite3
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtGui import *
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtGui import *
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('q.ui', self)
+        uic.loadUi('main.ui', self)
         self.connection = sqlite3.connect("coffee.db")
         self.pushButton.clicked.connect(self.pain)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pedit.clicked.connect(self.edit)
+        self.padd.clicked.connect(self.add)
 
     def pain(self):
         title1 = self.connection.cursor().execute(f"""SELECT id FROM Shows
@@ -36,6 +39,52 @@ class MyWidget(QMainWindow):
         self.label_12.setText(str(*nomer[0]))
         self.label_13.setText(str(*massa[0]))
         self.label_14.setText(*level[0])
+
+    def edit(self):
+        id = self.lineEdit.text()
+        tit = self.lineEdit_2.text()
+        obz = self.lineEdit_3.text()
+        zer = self.lineEdit_4.text()
+        des = self.lineEdit_5.text()
+        pr = self.lineEdit_6.text()
+        vol = self.lineEdit_7.text()
+        if obz:
+            self.connection.cursor().execute(f"""UPDATE Shows
+                    SET [степень обжарки] = '{obz}'
+                WHERE id = '{id}'""")
+            self.connection.commit()
+        if zer:
+            self.connection.cursor().execute(f"""UPDATE Shows
+                                SET [молотый/в зернах] = '{zer}'
+                            WHERE id = '{id}'""")
+            self.connection.commit()
+        if des:
+            self.connection.cursor().execute(f"""UPDATE Shows
+                                            SET [описание вкуса] = '{des}'
+                                        WHERE id = '{id}'""")
+            self.connection.commit()
+        if pr:
+            self.connection.cursor().execute(f"""UPDATE Shows
+                                                        SET цена = '{pr}'
+                                                    WHERE id = '{id}'""")
+            self.connection.commit()
+        if vol:
+            self.connection.cursor().execute(f"""UPDATE Shows
+                                                                    SET [объем упаковки] = '{vol}'
+                                                                WHERE id = '{id}'""")
+            self.connection.commit()
+
+    def add(self):
+        id = self.lineEdit.text()
+        tit = self.lineEdit_2.text()
+        obz = self.lineEdit_3.text()
+        zer = self.lineEdit_4.text()
+        des = self.lineEdit_5.text()
+        pr = self.lineEdit_6.text()
+        vol = self.lineEdit_7.text()
+        par = (id, tit, obz, zer, des, pr, vol)
+        self.connection.cursor().executemany(f"INSERT INTO users VALUES(?, ?, ?, ?, ?, ?, ?)", par)
+        self.connection.commit()
 
 
 if __name__ == '__main__':
